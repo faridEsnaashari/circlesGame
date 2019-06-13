bee8ef90dd92504471a9402ba0d5a8c25af293f5
fard2000 <47324985+fard2000@users.noreply.github.com>
2019-06-13 12:27:12 +0430

    Update init.js

--- init.js
+++ init.js
@@ -1,7 +1,18 @@
-document.addEventListener("DOMContentLoaded",function(){
-    document.querySelector("svg").style.display="none";
-    document.querySelector("#timerShowSec").style.display="none";
-    document.querySelector("#timerShowMin").style.display="none";
-    document.querySelector("#seprator").style.display="none";
-    document.querySelector("#gameOver").style.display="none";
-});
+ginline_id=null;
+guser_id=null;
+function init(inline_id,user_id){
+    // document.addEventListener("DOMContentLoaded",function(){
+        document.querySelector("svg").style.display="none";
+        document.querySelector("#timerShowSec").style.display="none";
+        document.querySelector("#timerShowMin").style.display="none";
+        document.querySelector("#seprator").style.display="none";
+        document.querySelector("#gameOver").style.display="none";
+        saveUserData(inline_id,user_id);
+    // });
+}
+
+
+function saveUserData(inline_id,user_id){
+    ginline_id=inline_id;
+    guser_id=user_id;
+}
